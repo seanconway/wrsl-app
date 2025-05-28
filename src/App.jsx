789308c0965d740793705edd483897869650eee2
Sean Conway <sean_conway@me.com>
2025-05-27 23:27:24 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-900 text-white text-3xl">
+      Hello, Scoreboard!
+    </div>
+  );
+}
+
+export default App;
